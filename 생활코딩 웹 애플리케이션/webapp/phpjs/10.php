af37f036db6089acd0a212c511937e6d883050e7
Seungju Kim <hgrace127@naver.com>
2017-03-21 23:21:11 +0900

    PHP Practice

---
+++ 생활코딩 웹 애플리케이션/webapp/phpjs/10.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>JavaScript</h1>
+	<script type="text/javascript">
+		list = new Array("one", "two", "three");
+		document.write(list[2]);
+		document.write(list.length);
+	</script>
+
+	<h1>php</h1>
+	<?php
+		$list = array("one", "two", "three");
+		echo $list[2];
+		echo count($list);
+	?>
+</body>
+</html>
