8bf0660273e9d48268f37d95425f19fa25d70235
Seungju Kim <hgrace127@naver.com>
2017-03-21 16:34:03 +0900

    웹 애플리케이션
    생활코딩 웹 애플리케이션 코드

---
+++ 생활코딩 웹 애플리케이션/webapp/phpjs/1.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+	<meta charset="utf-8">
+</head>
+<body>
+	<h1>php</h1>	
+	<?php
+		echo("Hello world!")
+	?>
+	<h1>JavaScript</h1>
+	<script>
+		document.write("Hello world!")
+	</script>		
+</body>
+</html>
---
+++ 생활코딩 웹 애플리케이션/webapp/phpjs/3.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<meta charset="utf-8">
+</head>
+<body>
+	<script type="text/javascript">
+		document.write("Hello world!");
+	</script>
+	<?php
+	?>
+</body>
+</html>
